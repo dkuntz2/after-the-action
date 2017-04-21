1967e47aba3f8f3237d845c10c869b8f69c260f7
Don Kuntz <don@kuntz.co>
2017-04-21 00:05:05 -0500

    Add column lookups to models

--- after_action/models/__init__.py
+++ after_action/models/__init__.py
@@ -1,9 +1,23 @@
 # from .user import User
 from orator import Model
 from orator.orm import has_many, belongs_to
+import inflection
 
 class _Model(Model):
     __primary_key__ = 'pk'
+
+    @classmethod
+    def columns(cls):
+        connection = cls.resolve_connection()
+        columns = connection.get_schema_manager().list_table_columns(cls.__table__ or inflection.tableize(cls.__name__))
+        return {name: column.get_type() for name, column in columns.items()}
+
+    # HACK!!!
+    #   Done to allow templates to use column names as a variable
+    #       {% set column = 'pk' %}
+    #       {{ user[column] }}
+    def __getitem__(self, item):
+        return getattr(self, item)
 
 class User(_Model):
     @has_many("user_fk")
